d7856a8c26997b65edda1ba366d2baee5e3e6b36
pvenkatesh <pvenkatesh4287@gmail.com>
2024-01-09 20:05:59 +0530

    installing pck

---
+++ install_packges.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ $ID -ne 0 ]
+then
+    echo  -e "$R Error:: please run this script with roor access $N"
+    exit 1
+else
+    echo "You are root user"
+fi
+
+echo "All arguments passed: $@"
